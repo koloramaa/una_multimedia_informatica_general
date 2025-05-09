5743d824a3c24132f2c3c943fe58e1f0acb38ada
Tomas <tibargad@gmail.com>
2025-05-09 20:08:14 +0000

    Este es un nuevo commit que medio que funciona!!!

--- main.cpp
+++ main.cpp
@@ -1,224 +1,350 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
+
+  bool salir_del_juego = false;
   bool gameover;
-  int opcion;
-  int vidas = 1;
-  char avanzar;  // `s` y `n` para avanzar a siguiente pantalla "quieres continuar?"
-  bool IniciarJuego; // En vez de modo facil del ejemplo. Habra modo INICIAR juego verdadero e iniciar juego falso
-  char salir; // o un salir
-   
-  gameover = false;
+  int opcion;   // Para el menú principal
+  int eleccion; // Para las elecciones del juego
 
 
-  while (vidas > 0) // Nos va a ser más simple manejarnos con la variable vidas en vez de gameover. Cuando vidas = 0 ahí tiramos pantalla de gameover.
+
+  while (!salir_del_juego)
+
   {
     do
     {
-      // Sistema de validación
-        system("clear");
-        cout << "Estás en el MENÚ... Elige una opción " << endl;
-        // agregamos las opciones
-        cout << "1 - Jugar\n";
-        cout << "2 - Créditos\n"; // para ingresar nuestros nombres y como jugar
-        cout << "Ingrese Opción: ";
-        cin >> opcion;
-    } while (opcion != 1 && opcion != 2);
-       
-    system("clear");
-    switch (opcion)
-        {
-         
-        case 1:
-          cout << "Elegiste iniciar el juego" << endl;
-          IniciarJuego = true;  
-          break;
-        case 2:  
-          cout << " CRÉDITOS. JUEGO ASOMBROSO HECHO POR CAMILO, DULCE, LOURDES Y TOMAS! " << endl;
-          
-          //IniciarJuego = false; tenemos dudas, no sabemos si va
-          break;
-        default:
-        break;
-          // cout << "¡Opción incorrecta!\n" << endl;
-        }
-
-     while (vidas >=1)
-     {
-      cout << "EL ETERNAUTA. Juego Iniciado" << endl;
-     
-      if (IniciarJuego)
+      system("clear");
+      cout << "EMPEZAR EL JUEGO (PRESIONE 1)" << endl;
+      cout << "SALIR (PRESIONE 2)" << endl;
+      cin >> opcion;
+      switch (opcion)
       {
-        cout << "EMPEZÓ LA AVENTURA. PANTALLA 1" << endl;
-        cout << "Querès avanzar? (s/n)" << endl;
-        cin >> avanzar;
-        if (avanzar == 's' || == 'S') // Si ponemos un ; acá terminamos la secuencia. No se ejecuta el if.
-        {
-          cout << "Dale, AVANZAMOS A la PANTALLA 2" << endl;
-          vidas = 1;
-        }
-
-        else if ( avanzar == 'n' || == 'N'); // el problema que teníamos acá es que no habíamos puesto == en ambos lados
-
-        {
-          cout << "Decidiste salir a la nieve." << endl;
+      case 1:
+        system("clear");
+        cout << "El juego está por comenzar..." << endl;
+        cin.ignore().get();
+        break;
+      case 2:
+        system("clear");
+        cout << "Ha decidido salir del juego..." << endl;
+        cin.ignore().get();
+        break;
+        case 3:
+        system ("clear");
+        cout <<  "Créditos" << endl;
+        cin.ignore().get();
+        break;
         
-          vidas = 0;
-        }
+      default:
+        system("clear");
+        cout << "Introduzca una opción válida." << endl;
+        cin.ignore().get();
+        break;
+      }
+    } while (opcion != 1 && opcion != 2 && opcion != 3);
 
-        else // acá había un error de sintáxis. El else no lleva condiciones. El else if si.
+    if (opcion == 1)
+    {
+      gameover = false;
+      while (!gameover)
+      {
+
+        //-------------------------- Pantalla 0. Inicio del juego. ---------------------------
+        do
+        {
+          system("clear");
+          cout << R"(Sos un estudiante universitario que vive en un departamento del barrio de Almagro con algunos amigxs. 
+          Todo comienza cuando, una noche de diciembre mientras jugaban videojuegos, se corta la luz y notan que una nieve sospechosa
+           acecha la ciudad. Desde ese momento, tienen que tomar decisiones rápidas para descubrir qué está pasando)" << endl;
+          cout << "Elija una opción:" << endl;
+          cout << R"(OPCION 1: Salir a ver la nieve.
+          (Presione 1)
+          )" 
+          << endl;
+          cout << R"(OPCION 2: Te quedas en casa a pensar otras soluciones.
+          (Presione 2)
+          )" 
+          << endl;
+          cin >> eleccion;
+
+        } while (eleccion != 1 && eleccion != 2); 
+
+        //-------------------------- Pantalla 1 ---------------------------
+
+        if (eleccion == 1)
 
         {
-            cout << "Opción no válida. Intenta de nuevo." << endl;
+            //pantalla gameover
+            system("clear");
+            cout << "Saliste directo a la nieve. Puede parecer un cuento de hadas, pero la nieve te aniquila. Moriste." << endl;
+            
+            cin.ignore().get();
+
+             gameover = true;
+
+          } 
+
+        //-------------------------- Pantalla 2 ---------------------------
+
+        else if (eleccion == 2)
+
+        {
+
+          do
+
+          {
+
+            system("clear");
+            
+            cout << R"(Narrador: Decidiste quedarte en casa
+            Tom: ¡Hey! miren!! un gato en la nieve está muriendo
+            Lou: Pobrecito.
+            Narrador: Al parecer tienen que sobrevivir a la nieve. Busquen una solución pronto… Sino, todos van a morir como ese gato! 
+            Narrador: Tu amigo tarotista está muy preocupado. Intuye que puede ser algo peligroso debido a la muerte del gato..
+            Dulce: ¡Boludo! Puede ser.. ¿Qué hacemos?)"
+
+            << endl;
+
+            cout << R"(Todos te miran a vos para tomar una decisión. Estás nervioso pero sabes que cuentan con vos. Presioná 1 para LLENARTE DE DETERMINACIÓN
+            o presiona 2 para mirar hacia el piso y NO DECIR NADA.)" << endl;
+
+            cin >> eleccion;
+
+          } while (eleccion != 1 && eleccion != 2);
+
+
+
+          // Pantalla 2.1
+
+          if (eleccion == 1 || eleccion == 2)
+
+          {
+
+            do
+
+            {
+
+              system("clear");
+
+              cout << R"(Narrador: Tienen que crear un traje que les permita salir sin ser aniquiladxs por la nieve peligrosa!
+              Todxs empiezan a buscar distintos materiales u objetos en el departamento.)" << endl;
+
+              cout << R"(¿Con qué haces tu traje?
+
+              1) Cortina de baño 
+
+
+              2) Trapo de piso
+
+
+              3) Vestido de XV de tu hermana.)" << endl; 
+              // acá tendría que adquirirse uno de los materiales que va a influir en el resultado posterior
+
+              cout << "ElEGÍ UN MATERIAL PRESIONANDO EL NÚMERO CORRESPONDIENTE" << endl;
+
+              cin >> eleccion;
+
+            } while (eleccion != 1 && eleccion != 2 && eleccion != 3);
+
+
+
+            // Pantalla 2.1.1
+
+            if (eleccion == 1)
+
+            {
+
+              system("clear");
+
+              cout << "Este es el final de 2.1.1" << endl;
+
+              cin.ignore().get();
+
+              gameover = true;
+
+            }
+
+            // Pantalla 2.1.2
+
+            else if (eleccion == 2)
+
+            {
+
+              system("clear");
+
+              cout << "Este es el final de 2.1.2" << endl;
+
+              cin.ignore().get();
+
+              gameover = true;
+
+            }
+
+          }
+
+          // Pantalla 2.2
+
+          else if (eleccion == 2)
+
+          {
+
+            do
+
+            {
+
+              system("clear");
+
+              cout << "Esta es la pantalla 2.2 del juego.\n"
+
+                   << endl;
+
+              cout << "Elija una opción:" << endl;
+
+              cout << "OPCION 1 (Presione 1)" << endl;
+
+              cout << "OPCION 2 (Presione 2)" << endl;
+
+              cin >> eleccion;
+
+            } while (eleccion != 1 && eleccion != 2);
+
+
+
+            // Pantalla 2.2.1
+
+            if (eleccion == 1)
+
+            {
+
+              system("clear");
+
+              cout << "Este es el final de 2.2.1" << endl;
+
+              cin.ignore().get();
+
+              gameover = true;
+
+            }
+
+            // Pantalla 2.2.2
+
+            else if (eleccion == 2)
+
+            {
+
+              system("clear");
+
+              cout << "Este es el final de 2.2.2" << endl;
+
+              cin.ignore().get();
+
+              gameover = true;
+
+            }
+
+          }
 
         }
+
+        //-------------------------- Pantalla 3 ---------------------------
+
+        else if (eleccion == 3)
+
+        {
+
+          do
+
+          {
+
+            system("clear");
+
+            cout << "Esta es la pantalla 3 del juego.\n"
+
+                 << endl;
+
+            cout << "Elija una opción:" << endl;
+
+            cout << "OPCION 1 (Presione 1)" << endl;
+
+            cin >> eleccion;
+
+          } while (eleccion != 1);
+
+
+
+          // Pantalla 3.1
+
+          if (eleccion == 1)
+
+          {
+
+            system("clear");
+
+            cout << "Este es el final de 3.1" << endl;
+
+            cin.ignore().get();
+
+            gameover = true;
+
+          }
+
+        }
+
+        //-------------------------- Pantalla 4 ---------------------------
+
+        else if (eleccion == 4)
+
+        {
+
+          system("clear");
+
+          cout << "Este es el final de 4" << endl;
+
+          cin.ignore().get();
+
+          gameover = true;
+
+        }
+
       }
 
 
 
-      {
- 
-      cout << "La nieve radioactiva entra por tus poros. Es el final." << endl;
-      gameover = true;
- 
+      system("clear");
+
+
+
+      cout << "Game Over" << endl;
+
       cout << "Presione ENTER para continuar...";
-      cin.ignore().get();
-      system("clear");  
 
-     }
-
+      cin.get();
 
     }
 
+    else
+
+    {
+
+      salir_del_juego = true;
+
+    }
 
   }
+
+
+
+  system("clear");
+
+  cout << "Hasta la próxima!" << endl;
+
+
 
   return 0;
 
 }
-
-
-
-
-// EJEMPLO DE OTRO JUEGO HECHO CON SWITCH PARA VER SI SEGUIMOS NUESTRA ESTRUCTURA O CAMBIAMOS
-
-// //#include <iostream>
-// using namespace std;
-
-// int main() {
-//     int pantalla_actual = 0; // 0 = Menú
-//     int vidas = 3;
-//     char input;
-
-//     while (vidas > 0) {
-//         system("clear"); // Limpia la pantalla (usar "cls" en Windows)
-
-//         switch (pantalla_actual) {
-//             // ----------------------------
-//             // PANTALLA 0: MENÚ PRINCIPAL
-//             // ----------------------------
-//             case 0:
-//                 cout << "🔥 EXPEDICIÓN PELIGROSA 🔥\n";
-//                 cout << "1. Iniciar Aventura\n";
-//                 cout << "2. Instrucciones\n";
-//                 cout << "3. Salir\n";
-//                 cout << "Elige: ";
-//                 cin >> input;
-
-//                 if (input == '1') pantalla_actual = 1; // Ir a Pantalla 1
-//                 else if (input == '2') pantalla_actual = 4; // Ir a Instrucciones
-//                 else if (input == '3') vidas = 0; // Salir
-//                 break;
-
-//             // ----------------------------
-//             // PANTALLA 1: BOSQUE
-//             // ----------------------------
-//             case 1:
-//                 cout << "🌳 BOSQUE OSCURO 🌳\n";
-//                 cout << "Vidas: " << vidas << "\n\n";
-//                 cout << "Un lobo te ataca! ¿Qué haces?\n";
-//                 cout << "1. Pelear\n2. Huir\n";
-//                 cout << "Elige: ";
-//                 cin >> input;
-
-//                 if (input == '1') {
-//                     cout << "¡Ganaste el combate! Avanzas a la cueva.\n";
-//                     pantalla_actual = 2; // Ir a Pantalla 2
-//                 } else {
-//                     vidas--;
-//                     cout << "Escapaste, pero perdiste 1 vida.\n";
-//                 }
-//                 cin.ignore(); // Pausa
-//                 break;
-
-//             // ----------------------------
-//             // PANTALLA 2: CUEVA
-//             // ----------------------------
-//             case 2:
-//                 cout << "🕳️ CUEVA MISTERIOSA 🕳️\n";
-//                 cout << "Vidas: " << vidas << "\n\n";
-//                 cout << "Hay dos túneles:\n";
-//                 cout << "1. Izquierda (oscuro)\n2. Derecha (ruidos)\n";
-//                 cout << "Elige: ";
-//                 cin >> input;
-
-//                 if (input == '1') {
-//                     pantalla_actual = 3; // Ir a Pantalla 3 (Tesoro)
-//                 } else {
-//                     pantalla_actual = 5; // Ir a Pantalla 5 (Trampa)
-//                 }
-//                 break;
-
-//             // ----------------------------
-//             // PANTALLA 3: TESORO
-//             // ----------------------------
-//             case 3:
-//                 cout << "💎 SALA DEL TESORO 💎\n";
-//                 cout << "¡Encontraste un cofre! ¿Abrirlo?\n";
-//                 cout << "s/n: ";
-//                 cin >> input;
-
-//                 if (input == 's') {
-//                     cout << "¡GANASTE EL JUEGO! 🏆\n";
-//                     vidas = 0; // Termina el juego
-//                 } else {
-//                     pantalla_actual = 1; // Regresa al bosque
-//                 }
-//                 break;
-
-//             // ----------------------------
-//             // PANTALLA 4: INSTRUCCIONES
-//             // ----------------------------
-//             case 4:
-//                 cout << "📜 INSTRUCCIONES 📜\n";
-//                 cout << "Usa números para elegir opciones.\n";
-//                 cout << "Cada decisión afecta tu supervivencia.\n";
-//                 cout << "Presiona 's' para continuar...";
-//                 cin >> input;
-//                 pantalla_actual = 0; // Regresa al menú
-//                 break;
-
-//             // ----------------------------
-//             // PANTALLA 5: TRAMPA
-//             // ----------------------------
-//             case 5:
-//                 cout << "💀 TRAMPA MORTAL 💀\n";
-//                 cout << "¡Caíste en un pozo!\n";
-//                 vidas = 0; // Game Over
-//                 break;
-//         }
-
-//         // Pausa entre pantallas
-//         if (vidas > 0 && pantalla_actual != 0) {
-//             cout << "\nPresiona Enter para continuar...";
-//             cin.ignore();
-//             cin.get();
-//         }
-//     }
-
-//     cout << "FIN DEL JUEGO\n";
-//     return 0;
-// }
